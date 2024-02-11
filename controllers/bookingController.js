292807c8886dfd08f22eade0c237b9e409126148
ItayMz <79039985+ItayMz@users.noreply.github.com>
2024-02-11 15:27:42 +0200

    Modified booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,6 +5,14 @@ const Booking = require('../models/bookingModel');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 const User = require('../models/userModel');
+
+async function createBookingCheckout(session) {
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
+ 
+  await Booking.create({ tour, user, price });
+}
 module.exports = {
   createBooking: factory.createOne(Booking),
   getBooking: factory.getOne(Booking),
@@ -24,7 +32,7 @@ module.exports = {
       // success_url: `${req.protocol}://${req.get('host')}/?tour=${
       //   req.params.tourId
       // }&user=${req.user.id}&price=${tour.price}`,
-      success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+      success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
       customer_email: req.user.email,
       client_reference_id: req.params.tourId,
@@ -41,7 +49,11 @@ module.exports = {
             product_data: {
               name: `${tour.name} Tour`,
               description: tour.summary,
-              images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+              images: [
+                `${req.protocol}://${req.get('host')}/img/tours/${
+                  tour.imageCover
+                }`,
+              ],
             },
           },
         },
@@ -65,30 +77,25 @@ module.exports = {
 
   //   res.redirect(req.originalUrl.split('?')[0]);
   // }),
-  createBookingCheckout: async function(session){
-    const tour = session.client_reference_id;
-    const user = (await User.findOne({email: session.customer_details.email})).id
-    const price = session.amount_total/100;
-    await Booking.create({ tour, user, price }); 
-  },
   webhookCheckout: function (req, res, next) {
     const signature = req.headers['stripe-signature'];
     let event;
-    try{
-       event = stripe.webhooks.constructEvent(
+    try {
+      event = stripe.webhooks.constructEvent(
         req.body,
         signature,
         process.env.STRIPE_WEBHOOK_SECRET
       );
-    }catch(err){
-      return res.status(400).send(`Webhook error: ${err.message}`)
+    } catch (err) {
+      return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if(event.type === 'checkout.session.completed') 
-      createBookingCheckout(event.data.object)
+    if (event.type === 'checkout.session.completed'){
+      createBookingCheckout(event.data.object);
 
-    res.status(200).json({received: true})
-    
+    }
+
+    res.status(200).json({ received: true });
   },
   checkIfBooked: catchAsync(async function (req, res, next) {
     const bookings = await Booking.find({
