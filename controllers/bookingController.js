0a39f8e67dedbeabfbd863ba8438db77107e3922
ItayMz <79039985+ItayMz@users.noreply.github.com>
2024-02-11 13:48:03 +0200

    Imoroved stripe implementation again

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -41,7 +41,7 @@ module.exports = {
             product_data: {
               name: `${tour.name} Tour`,
               description: tour.summary,
-              images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+              images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
             },
           },
         },
